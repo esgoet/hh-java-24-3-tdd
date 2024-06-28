114807faccbc74f5f5605b7f00b8ae641d1bf9f6
Simon Staß <simonsta95@gmail.com>
2024-06-28 17:01:35 +0200

    add fibonacci aufgabe

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,5 +1,20 @@
 public class Fibonacci {
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(6));
+    }
+
     public static int fibonacci(int n) {
-        return 1;
+
+
+        if (n == 0){
+            return 0;
+        }
+        if (n == 1){
+            return 1;
+        }
+
+        return fibonacci(n-1) + fibonacci(n-2);
+
     }
 }
--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -5,9 +5,45 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FibonacciTest {
 
     @Test
-    public void fibonacciTest_when0_thenReturn1() {
+    public void fibonacciTest_when0_thenReturn0() {
         //GIVEN
         int num = 0;
+
+        //WHEN
+        int actual = Fibonacci.fibonacci(num);
+
+        //THEN
+        assertEquals(0,actual);
+    }
+
+    @Test
+    public void fibonacciTest_when1_thenReturn1() {
+        //GIVEN
+        int num = 1;
+
+        //WHEN
+        int actual = Fibonacci.fibonacci(num);
+
+        //THEN
+        assertEquals(1,actual);
+    }
+
+    @Test
+    public void fibonacciTest_when2_thenReturn1() {
+        //GIVEN
+        int num = 2;
+
+        //WHEN
+        int actual = Fibonacci.fibonacci(num);
+
+        //THEN
+        assertEquals(1,actual);
+    }
+
+    @Test
+    public void fibonacciTest_when6_thenReturn8() {
+        //GIVEN
+        int num = 1;
 
         //WHEN
         int actual = Fibonacci.fibonacci(num);
